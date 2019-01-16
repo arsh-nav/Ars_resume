d425eb2501105add0d6b7ee76f8b553ea583e614
arsh-nav <arshia.nawab@gmail.com>
2019-01-16 23:28:30 +0330

    Updates

---
+++ precache-manifest.4048998f7f0f63dc59d4746046b2bc88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a61d5cd3ce15e54c1ce",
+    "url": "/Ars_resume/static/css/main.42642639.chunk.css"
+  },
+  {
+    "revision": "4a61d5cd3ce15e54c1ce",
+    "url": "/Ars_resume/static/js/main.4a61d5cd.chunk.js"
+  },
+  {
+    "revision": "f4ec1d774bdf7d24c24d",
+    "url": "/Ars_resume/static/css/1.f09ea8a2.chunk.css"
+  },
+  {
+    "revision": "f4ec1d774bdf7d24c24d",
+    "url": "/Ars_resume/static/js/1.f4ec1d77.chunk.js"
+  },
+  {
+    "revision": "111808cf4f6f71fa0822",
+    "url": "/Ars_resume/static/js/runtime~main.111808cf.js"
+  },
+  {
+    "revision": "6d06227393b1b872c15cb92408bc139d",
+    "url": "/Ars_resume/index.html"
+  }
+];
